9cdb26c731790db43aa543b0e5fc67e6ffc3ee37
amarpustam45 <amarpustam@hotmail.com>
2022-11-12 23:24:06 -0400

    trying with cors again

--- index.js
+++ index.js
@@ -14,7 +14,15 @@ const app = express();
 
 app.use(express.json({ limit: '50mb' }));
 app.use(cookieParser());
-app.use(function (req, res, next) {
+app.use(
+  cors({
+    credentials: true,
+    origin: 'http://localhost:3000',
+    // origin: 'https://amarpustam-blog-app-demo.netlify.app',
+  })
+);
+
+app.use((req, res, next) => {
   res.header('Access-Control-Allow-Credentials', true);
   res.header('Access-Control-Allow-Origin', req.headers.origin);
   res.header(
@@ -27,14 +35,6 @@ app.use(function (req, res, next) {
   );
   next();
 });
-
-app.use(
-  cors({
-    credentials: true,
-    origin: 'http://localhost:3000',
-    // origin: 'https://amarpustam-blog-app-demo.netlify.app',
-  })
-);
 
 app.use('/api/uploadCloud', uploadRoutes);
 app.use('/api/auth', authRoutes);
